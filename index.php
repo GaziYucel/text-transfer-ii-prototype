47c0820c2f7d8994cc97e2217ecc26fc51ae6d19
GaziYucel <GaziYucel@noreply>
2023-09-29 14:57:39 +0200

    separate header/footer gives errors in vuejs

--- index.php
+++ index.php
@@ -1,4 +1,36 @@
-<?php require_once('_header.php'); ?>
+<?php require_once('config.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Text Transfer II</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+
+    <!--<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">-->
+    <link rel="stylesheet" type="text/css" href="assets/lib/bootstrap.min.css">
+
+    <!--<script src="//unpkg.com/vue@3/dist/vue.global.prod.js"></script>-->
+    <script type="text/javascript" src="assets/lib/vue.global.prod.js"></script>
+
+    <!--<script src="//github.com/foliojs/pdfkit/releases/download/v0.12.0/pdfkit.standalone.js"></script>-->
+    <script type="text/javascript" src="assets/lib/pdfkit.standalone.js"></script>
+
+    <!--<script src="//github.com/devongovett/blob-stream/releases/download/v0.1.3/blob-stream.js"></script>-->
+    <script type="text/javascript" src="assets/lib/blob-stream.js"></script>
+
+    <script type="text/javascript" src="assets/js/script.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+
+<header class="row">
+    <div class="col-12">
+        <p><a href="index.php"><img class="logo" src="assets/img/logo-small.png" alt="TextTransfer Logo"/></a></p>
+        <hr/>
+    </div>
+</header>
+
 
 <main class="row">
     <div class="col-12">
@@ -26,4 +58,15 @@
     </div>
 </main>
 
-<?php require_once('_footer.php'); ?>
+<footer class="row">
+    <div class="col-12">
+        <hr/>
+        <a href="https://texttransfer.org/contact.html" target="_blank">Contact</a> |
+        <a href="https://texttransfer.org/en/privacy-policy.html" target="_blank">Privacy Policy</a> |
+        <a href="https://texttransfer.org/en/legal-notice.html" target="_blank">Legal Notice</a> |
+        <a href="https://github.com/TIBHannover/text-transfer-ii-prototype" target="_blank">Code (GitHub)</a>
+    </div>
+</footer>
+
+</body>
+</html>
